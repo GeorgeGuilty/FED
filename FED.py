0d2fa9db1f44bbb0b4dde300dc538a1497609f10
George Guilty <geoguiltycorreo@gmail.com>
2023-02-02 00:37:13 -0600

    Update FED.py
    Mejora mayor donde ya logra obtener esquinas más definidas. Se requieren más pruebas

--- FED.py
+++ FED.py
@@ -1,42 +1,14 @@
 import cv2
-from matplotlib import pyplot as plt
 import numpy as np
 
-def exactEdgePixel(img,img_max,s,mu):
-    if s % 2 != 1 and s < 3:
-        raise Exception('s should be an odd matrix size more than 3x3')
-
-    img_e = img_max
-    new_img = img_e
-
-    while np.sum(new_img) > 0:
-        new_img = np.zeros(img_e.shape,dtype= 'int32')
-        for i in range(img.shape[0]-s+1):
-            for j in range(img.shape[1]-s+1):
-                tmE = img_e[i:(i+s),j:(j+s)]
-                tmf = img[i:(i+s),j:(j+s)]
-                t1 = 0 == img_e[i+(s-1)//2,j+(s-1)//2]
-                t2 = np.sum(np.where(tmE >0,1,0)) >= 1
-                t3 = np.sum(np.where(tmE >0,1,0)) <= s
-                t4 = np.argsort(tmf,axis=None)[(s**2-1)//2] >= s*s -s
-                t5 = img[i+(s-1)//2,j+(s-1)//2]>=mu*tmf.max()
-                if t1 and t2 and t3 and t4 and t5:
-                    new_img[i+(s-1)//2,j+(s-1)//2] = 1
-        img_e += new_img*img_e.max()
-        imgshow = (img_e-np.min(img_e))/(np.max(img_e)-np.min(img_e))
-        cv2.imshow('exact edge',imgshow)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-    return img_e
-
-img = cv2.imread('Ex1.png', 0)
+img = cv2.imread('Ex2.jpeg', 0)
 dft = cv2.dft(np.float32(img), flags=cv2.DFT_COMPLEX_OUTPUT)
 dft_shift = np.fft.fftshift(dft)
 
 u,v = np.meshgrid(range(dft.shape[1]),range(dft.shape[0]))
 d = np.sqrt((u-dft.shape[1]/2)**2+(v-dft.shape[0]/2)**2)+1
 alpha = 1
-d0 = 20
+d0 = 200
 n = 1
 h = 1/(1+alpha*((d0/d)**(2*n)))
 h = np.stack((h,h),axis=2)
@@ -51,32 +23,28 @@ cv2.imshow('Highpass filter',imgshow)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-gamma = 0.2
-img_back = np.where(img_back >= gamma*img_back.max(), img_back,0)
-
 imgshow = (img_back-np.min(img_back))/(np.max(img_back)-np.min(img_back))
 cv2.imshow('Artifact Suppresion',imgshow)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-# g,p = gradient(img_back)
-# img_max = maximum(g, p)
+img_blur = cv2.GaussianBlur(img_back,(3,3),0)*-1
+img_supr = cv2.Laplacian(img_blur,cv2.CV_64FC1)
 
-kernel1 = np.array([[1, 1, 1],
-                    [1, 0, 1],
-                    [1, 1, 1]])
- 
-img_sup = (cv2.filter2D(src=img_back,ddepth=-1, kernel=kernel1)/9)-img_back
+imgshow = ((img_supr-np.min(img_supr))/(np.max(img_supr)-np.min(img_supr))*255).astype(np.uint8)
 
-imgshow = (img_sup-np.min(img_sup))/(np.max(img_sup)-np.min(img_sup))
-cv2.imshow('Maxima Suppresion',imgshow)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+v, c = np.unique(imgshow,return_counts=True)
 
-final = exactEdgePixel(img_back,img_sup,3,0.4)
+thr = (v[np.argsort(c)[-1]]/255)*(np.max(img_supr)-np.min(img_supr))+np.min(img_supr)
 
-final = (final-np.min(final))/(np.max(final)-np.min(final))
+gamma = 0.1
 
-cv2.imshow('final',final)
+img_thr = np.where(img_supr >= thr, img_supr,0)
+
+img_thr = np.where(img_supr >= gamma*img_supr.max(), 1,0)
+
+imgshow = (img_thr-np.min(img_thr))/(np.max(img_thr)-np.min(img_thr))
+
+cv2.imshow('Ringing Suppresion',imgshow)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
