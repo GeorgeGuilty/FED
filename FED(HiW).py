1ca9eafcd384a23a6b540350bcad277cac9f95f7
George Guilty <geoguiltycorreo@gmail.com>
2023-02-11 00:11:01 -0600

    Update FED
    Se intentó utilizar mejores algoritmos que puedan limpiar el ruido más la unión de los bordes

--- FED(HiW).py
+++ FED(HiW).py
@@ -1,9 +1,16 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('Ex2.png', 0)
-img = cv2.GaussianBlur(img,(3,3),0)
+img = cv2.imread('Ex1.png', 0)
 cv2.imshow('Original',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+img = cv2.GaussianBlur(img,(3,3),0)
+k = -np.ones((7,7))
+k[7//2,7//2] = 7*7
+img = cv2.filter2D(img,-1,k)
+cv2.imshow('Gaussian Blur and Sharpening',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
@@ -34,36 +41,47 @@ cv2.destroyAllWindows()
 f_ishift = np.fft.ifftshift(fh)
 img_back = cv2.idft(f_ishift)
 margin = np.ones_like(img)
-margin[0:5,:] = 0
-margin[-1:-6:-1,:] = 0
-margin[:,0:5] = 0
-margin[:,-1:-6:-1] = 0
+margin[0:7,:] = 0
+margin[-1:-8:-1,:] = 0
+margin[:,0:7] = 0
+margin[:,-1:-8:-1] = 0
 img_back = cv2.magnitude(img_back[:, :, 0], img_back[:, :, 1])*margin
 
 imgshow = (img_back-np.min(img_back))/(np.max(img_back)-np.min(img_back))
-cv2.imshow('Highpass filter',imgshow)
+cv2.imshow('Highpass filtered',imgshow)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-gamma = 0.05
-
-img_thr = np.where(img_back >= gamma*img_back.max(), img_back,0)
-
+gamma = 0.2
+img_thr = np.where(img_back >= gamma*img_back.max(),img_back,0)
 imgshow = (img_thr-np.min(img_thr))/(np.max(img_thr)-np.min(img_thr))
-cv2.imshow('Thresholding 1',imgshow)
+cv2.imshow('Threshold 1',imgshow)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-img_blur = cv2.GaussianBlur(img_thr,(13,13),0)
+img_blur = cv2.GaussianBlur(img_thr,(3,3),0)
 
 imgshow = (img_blur-np.min(img_blur))/(np.max(img_blur)-np.min(img_blur))
-cv2.imshow('Gaussian Blur',imgshow)
+cv2.imshow('Gaussian Blur 2',imgshow)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-img_thr = ((img_blur-np.min(img_blur))/(np.max(img_blur)-np.min(img_blur))*255).astype(np.uint8)
-img_thr = cv2.adaptiveThreshold(img_thr,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,3,-0.7)
+img_mul = (img_thr**0.5)*img_blur
+
+imgshow = (img_mul-np.min(img_mul))/(np.max(img_mul)-np.min(img_mul))
+cv2.imshow('Filter blur element multiplication',imgshow)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+img_thr = ((img_mul-np.min(img_mul))/(np.max(img_mul)-np.min(img_mul))*255).astype(np.uint8)
+img_thr = cv2.adaptiveThreshold(img_thr,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,3,-15)
 
 cv2.imshow('Thresholding 2',img_thr)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+img_mcl = cv2.morphologyEx(img_thr,cv2.MORPH_CLOSE,np.ones((2,2)))
+imgshow = (img_mcl-np.min(img_mcl))/(np.max(img_mcl)-np.min(img_mcl))
+cv2.imshow('morph close',imgshow)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- FED.py
+++ FED.py
@@ -1,11 +1,14 @@
 import cv2
 import numpy as np
 
-def FED(img, d0 = 1000,m = 5,g=0.05,bks1=13,bks2=3,cc=-1):
+def FED(img, d0 = 1000,m = 7,sk = 5,g = 0.3,bks1 = 3,bks2=3, bks3=3,cc=-15,mk=2):
     if len(img.shape) > 2 or len(img.shape) == 0:
         raise ValueError('Only grayscale images are allowed')
-    # Import Image and lil blur
-    img = cv2.GaussianBlur(img,(3,3),0)
+    # Blur and sharp image to clean and prepare for a better edge detection
+    img = cv2.GaussianBlur(img,(bks1,bks1),0)
+    k = -np.ones((sk,sk))
+    k[sk//2,sk//2] = sk*sk
+    img = cv2.filter2D(img,-1,k)
     # DFT
     dft = cv2.dft(np.float32(img), flags=cv2.DFT_COMPLEX_OUTPUT)
     dft_shift = np.fft.fftshift(dft)
@@ -24,13 +27,15 @@ def FED(img, d0 = 1000,m = 5,g=0.05,bks1=13,bks2=3,cc=-1):
     margin[:,0:m] = 0
     margin[:,-1:(-1-m):-1] = 0
     img_back = cv2.magnitude(img_back[:, :, 0], img_back[:, :, 1])*margin
-    # Remove noise and apply blur for wider edges
-    img_thr = np.where(img_back >= g*img_back.max(), img_back,0)
-    img_blur = cv2.GaussianBlur(img_thr,(bks1,bks1),0)
-    # Apply threshold and final image is delivered
-    img_thr = ((img_blur-np.min(img_blur))/(np.max(img_blur)-np.min(img_blur))*255).astype(np.uint8)
-    img_thr = cv2.adaptiveThreshold(img_thr,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,bks2,cc)
-    return img_thr
+    # Apply first threshold and gaussian blur to join edges and blur noise and join both
+    img_thr = np.where(img_back >= g*img_back.max(),img_back,0)
+    img_blur = cv2.GaussianBlur(img_thr,(bks2,bks2),0)
+    img_mul = (img_thr**0.5)*img_blur
+    # Apply second threshold with adaptive gaussian and final image is delivered
+    img_uint = ((img_mul-np.min(img_mul))/(np.max(img_mul)-np.min(img_mul))*255).astype(np.uint8)
+    img_thr = cv2.adaptiveThreshold(img_uint,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,bks3,cc)
+    img_mcl = cv2.morphologyEx(img_thr,cv2.MORPH_CLOSE,np.ones((mk,mk)))
+    return img_mcl
 
 if __name__ == '__main__':
     # img = cv2.imread('Ex1.png',0)
@@ -47,7 +52,8 @@ if __name__ == '__main__':
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
         # Display the resulting frame
-        cv2.imshow('frame',  FED(gray))
+        cv2.imshow('frame',  frame)
+        cv2.imshow('FED',  FED(gray))
         # the 'q' button is set as the
         # quitting button you may use any
         # desired button of your choice
